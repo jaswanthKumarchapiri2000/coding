c7fab1ffb6eb8992ae7bb8cadf44b30eece6521b
jaswanthKumarchapiri2000 <47524991+jaswanthKumarchapiri2000@users.noreply.github.com>
2022-11-04 14:02:01 +0530

    Added solution - LeetHub

---
+++ Base Equivalence - GFG/base-equivalence.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+class Solution:
+	def baseEquiv(self, n, m):
+	    if m==1 and n <=32:
+	        return 'Yes'
+	    def helper(num,base):
+	        s=''
+	        while(num!=0):
+	            basedigit=num%base
+	            s='a'+s
+	            num=num//base
+	        return s
+	    for i in range(2,33):
+	        if len(helper(n,i))==m:
+	            return 'Yes'
+	    return 'No'
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n,m = input().split()
+		n=int(n)
+		m=int(m)
+		ob = Solution();
+		print(ob.baseEquiv(n,m))
+
+# } Driver Code Ends
