7974ed5fa41e31f40136291e1fe008980ee6aa9e
jaswanthKumarchapiri2000 <47524991+jaswanthKumarchapiri2000@users.noreply.github.com>
2023-02-04 10:24:54 +0530

    Added solution - LeetHub

---
+++ Last index of One - GFG/last-index-of-one.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    def lastIndex(self, s):
+        # code here
+        length=len(s)
+        for i in range(length-1,-1,-1):
+            if s[i]=='1':
+                return i
+        return -1
+        
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+    	s = input()
+    	ob = Solution()
+    	print(ob.lastIndex(s))
+    	
+    	T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
