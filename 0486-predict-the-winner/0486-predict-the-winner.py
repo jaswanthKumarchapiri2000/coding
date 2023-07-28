51fff717f5920b3e08f4cd494104db1cf45ed1a9
jaswanthKumarchapiri2000 <47524991+jaswanthKumarchapiri2000@users.noreply.github.com>
2023-07-28 22:08:32 +0530

    Time: 41 ms (96.36%), Space: 16.4 MB (70.15%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,21 +1,15 @@
 from functools import cache
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        total=sum(nums)
         n=len(nums)
-        @cache
-        def dfs(start,end):
-            if start > end:
-                # print(start,end,score1,score2)
-                return 0
-            score1=max(nums[start]-dfs(start+1,end),nums[end]- dfs(start,end-1))
-            return score1
-             
-        if dfs(0,n-1)>=0:
-            return True
-        return False
-        
-            
+        dp=[[0 for i in range(n+1)] for j in range(n+1)]
+        for i in range(n):
+            for j in range(n-i):
+                # print(j,j+i, end= " here   ")
+                dp[j][j+i]=max(nums[j]-dp[j+1][j+i],nums[j+i]-dp[j][j+i-1])
+            # print(dp)
+        # print(dp)
+        return dp[0][n-1]>=0
                         
 
             
