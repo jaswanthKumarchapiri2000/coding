14b4e8fc56d30a7d4f97a5fe63042583610c70e4
jaswanthKumarchapiri2000 <47524991+jaswanthKumarchapiri2000@users.noreply.github.com>
2023-08-01 10:47:23 +0530

    Time: 278 ms (79.08%), Space: 18.1 MB (90.06%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,20 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def generate_combinations(elems, num):
-            elems_tuple = tuple(elems)
-            total = len(elems_tuple)
-            if num > total:
-                return
-            curr_indices = list(range(num))
-            while True:
-                yield tuple(elems_tuple[i] for i in curr_indices)
-                for idx in reversed(range(num)):
-                    if curr_indices[idx] != idx + total - num:
-                        break
-                else:
-                    return
-                curr_indices[idx] += 1
-                for j in range(idx+1, num):
-                    curr_indices[j] = curr_indices[j-1] + 1
+        res = []
+        comb = []
 
-        return [list(combination) for combination in generate_combinations(range(1, n+1), k)]
+        def backtrack(start):
+            if len(comb) == k:
+                res.append(comb[:])
+                return
+            
+            for num in range(start, n + 1):
+                comb.append(num)
+                backtrack(num + 1)
+                comb.pop()
+
+        backtrack(1)
+        return res
