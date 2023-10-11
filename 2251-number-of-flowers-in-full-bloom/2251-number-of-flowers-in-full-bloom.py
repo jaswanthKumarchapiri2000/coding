366c6e213ffde59f193d5f2b84a02a302bd7ef83
jaswanthKumarchapiri2000 <47524991+jaswanthKumarchapiri2000@users.noreply.github.com>
2023-10-11 12:43:03 +0530

    Time: 879 ms (89.09%), Space: 44.8 MB (41.82%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -0,0 +1,7 @@
+from bisect import bisect_right,bisect_left
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        start, end = sorted(a for a,b in flowers), sorted(b for a,b in flowers)
+        return [bisect_right(start, t) - bisect_left(end, t) for t in people]
+            
+        
